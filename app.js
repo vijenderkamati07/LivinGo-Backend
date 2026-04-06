df1ba1e096d5c471119e8e1467337bafcebbbb3c
Vijender Kamati <vijenderkamati71@gmail.com>
2026-04-06 09:33:31 +0530

    Update app.js
    Fixing auto logout error.

--- app.js
+++ app.js
@@ -89,12 +89,12 @@ app.use(
     saveUninitialized: false,
     store: store,
     cookie: {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite:
-        process.env.NODE_ENV === "production" ? "none" : "lax",
-      maxAge: 1000 * 60 * 60 * 24 * 7,
-    },
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite:
+    process.env.NODE_ENV === "production" ? "none" : "lax",
+  maxAge: 1000 * 60 * 60 * 24 * 7,
+}
   })
 );
 
